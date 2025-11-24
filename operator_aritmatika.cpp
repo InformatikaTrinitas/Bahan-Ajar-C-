6dc7a975ff12a1450a7035cc4b3c81659283eb9b
Informatika SMA Trinitas <197331326+InformatikaTrinitas@users.noreply.github.com>
2025-11-24 11:45:05 +0700

    Create operator_aritmatika.cpp

---
+++ operator_aritmatika.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "=== Operator Aritmatika ===\n";
+    cout << "5 + 3 = " << 5 + 3 << endl;
+    cout << "5 - 3 = " << 5 - 3 << endl;
+    cout << "5 * 3 = " << 5 * 3 << endl;
+    cout << "10 / 2 = " << 10 / 2 << endl;
+    cout << "10 % 3 = " << 10 % 3 << endl;
+
+    return 0;
+}
