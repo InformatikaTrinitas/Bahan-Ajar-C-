4f92878aabfa93e2c2f2d71c0a4d3620d2b53589
Informatika SMA Trinitas <197331326+InformatikaTrinitas@users.noreply.github.com>
2025-12-02 21:21:16 +0700

    Create if_elseif_else.cpp

---
+++ if_elseif_else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int nilai;
+	cin>>nilai;
+	if(nilai>=90){
+		cout<<"A";
+	}else if(nilai >=80){
+		cout<<"B";
+	}else{
+		cout<<"C";
+	}
+	return 0;
+}
